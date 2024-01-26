c3ea0507e24bb3399f660ce9319db0fa5635b3d1
Francis Osei <francis@bayezian.com>
2024-01-26 15:53:21 +0000

    files

--- app.py
+++ app.py
@@ -10,39 +10,45 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain.agents import AgentExecutor, create_react_agent
 from langchain import hub
 from langchain.agents import AgentExecutor, create_openai_functions_agent
+from dotenv import load_dotenv
 import pandas as pd
 import numpy as np
 import os
 import streamlit as st
 
+os.environ["OPENAI_API_KEY"] = "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"
+def main():
+    load_dotenv()
 
-key = st.text_input("Enter your OpenAI key")
-os.environ["OPENAI_API_KEY"] = key
 
-user_input = st.text_input("Input your Query")
+    st.set_page_config(page_title="Ask your CSV")
+    st.header("Ask your CSV 📈")
 
-# File uploader for the parquet file
-data_file = st.file_uploader("Upload your Data")
-
-if data_file is not None:
-    with open(os.path.join(os.getcwd(), data_file.name), 'wb') as f:
-        f.write(data_file.getvalue())
-
-# Check if a file has been uploaded
-if data_file is not None:
-    with st.spinner("Analyzing data...⌛"):
-        data = pd.read_parquet(data_file)
-        df_reset = data.reset_index()
-        df_reset['symbol'] = df_reset['symbol'].ffill()
+    csv_file = st.file_uploader("Upload a CSV file")
+    if csv_file is not None:
+        with open(os.path.join(os.getcwd(), csv_file.name), 'wb') as f:
+             f.write(csv_file.getvalue())
+             
+        data = pd.read_parquet(csv_file)
+        document = data.reset_index()
+        document['symbol'] = document['symbol'].ffill()
+        
         prompt = ChatPromptTemplate.from_messages([
             ("system", "You are a AI financial stock market analyst. You are to provide a detail analysis of the data using the provided question"),
             ("user", "{input}")
             ])
         
-        llm = create_pandas_dataframe_agent(ChatOpenAI(temperature=0,model = "gpt-4-0613"), df_reset, verbose=True)
-        chain = prompt | llm
-        chain.invoke({"input": f"calculate divergence by TSLA % change - SPY % change"})
+        llm = create_pandas_dataframe_agent(ChatOpenAI(temperature=0,model = "gpt-4-0613"), document, verbose=False)
+        chain = prompt | llm 
 
+        user_question = st.text_input("Ask a question about your CSV: ")
+
+        if user_question is not None and user_question != "":
+            with st.spinner(text="In progress..."):
+                st.write(chain.invoke({"input": user_question}))
+                
+if __name__ == "__main__":
+    main()
 
 
 

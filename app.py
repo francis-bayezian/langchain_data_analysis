96fdde085a5b910fc41dadb9b571fc2974b68739
Francis Osei <francis@bayezian.com>
2024-01-27 12:17:13 +0000

    files

--- app.py
+++ app.py
@@ -2,15 +2,9 @@ from langchain_openai import OpenAI
 from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
-from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain.agents import AgentExecutor, create_react_agent
-from langchain import hub
-from langchain.agents import AgentExecutor, create_openai_functions_agent
 from dotenv import load_dotenv
+from PIL import Image
 import pandas as pd
 import numpy as np
 import os
@@ -21,11 +15,9 @@ import streamlit as st
 
 def main():
     load_dotenv()
-
-
     st.set_page_config(page_title="Ask your CSV")
-    st.header("Ask your CSV 📈")
-    key = st.text_input("Enter your OpenAI key")
+    st.title("Streamlit Financial Analyzer 📊")
+    key = st.text_input("Enter your OpenAI key",type="password")
     os.environ["OPENAI_API_KEY"] = key
 
     csv_file = st.file_uploader("Upload a CSV file")
@@ -38,19 +30,46 @@ def main():
         document['symbol'] = document['symbol'].ffill()
         
         prompt = ChatPromptTemplate.from_messages([
-            ("system", "You are a AI financial stock market analyst. You are to provide a detail analysis of the data using the provided question"),
+            ("system", "You are a AI financial stock market analyst. You are to provide a detail analysis of the data using the provided question."),
             ("user", "{input}")
             ])
         
+        st_callback = StreamlitCallbackHandler(st.container())
+        
         llm = create_pandas_dataframe_agent(ChatOpenAI(temperature=0,model = "gpt-4-0613"), document, verbose=False)
-        chain = prompt | llm 
-
+        chain = prompt | llm
+        
+        
         user_question = st.text_input("Ask a question about your CSV: ")
+        question =  user_question + f" . Plot and save the output"
 
         if user_question is not None and user_question != "":
             with st.spinner(text="In progress..."):
-                st.write(chain.invoke({"input": user_question}))
+                output = chain.invoke({"input": question })
+                desired_output = output['output']
+                message = st.chat_message("assistant")
+                message.write(desired_output)
                 
+                current_directory = os.getcwd()
+                files = os.listdir(current_directory)
+                image_files = [file for file in files if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+                for image_file in image_files:
+                    image_path = os.path.join(current_directory, image_file)
+                    image = Image.open(image_path)
+                    st.image(image, caption=image_file, use_column_width=True)
+                    
+                csv_files = [f for f in os.listdir(current_directory) if f.endswith('.csv')]
+                for csv_file in csv_files:
+                    csv_path = os.path.join(current_directory, csv_file)
+                    data_csv = pd.read_csv(csv_path)
+                    st.table(data_csv)
+        
+                    
+                files = [file for file in files if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif','.csv'))]
+                for file in files:
+                    image_path = os.path.join(current_directory, file)
+                    os.remove(image_path)
+            
 if __name__ == "__main__":
     main()
 

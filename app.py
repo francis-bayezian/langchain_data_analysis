dd82f47f3a9ec07dff08280748052314a71d9e4d
Francis Osei <francis@bayezian.com>
2024-01-27 13:30:58 +0000

    files

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from langchain_openai import OpenAI
 from langchain.agents.agent_types import AgentType
-from langchain_experimentalS.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
